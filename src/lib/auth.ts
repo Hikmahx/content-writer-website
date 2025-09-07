f41bf6433e7fd5a735008d05a7e1f043c71c466f
Hikmahx <hikmayousuph@gmail.com>
2025-09-07 20:58:30 +0100

    feat: add Prisma schema and update authentication setup
    - Added Prisma schema for MongoDB with User, Account, Session, VerificationToken, and Authenticator models.
    - Updated `auth.ts` to use PrismaAdapter from `@auth/prisma-adapter`.
    - Created `prisma.ts` for Prisma client initialization.
    - Updated dependencies in `package.json` and `package-lock.json` for Prisma and authentication packages.
    - Added generated Prisma files to `.gitignore`.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,13 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from './prisma'
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_ID!,
-        clientSecret: process.env.GOOGLE_SECRET!,
-    })
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
   ],
 }
