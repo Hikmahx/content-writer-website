67c8c3e0ba276748b1781484c20c3fe8f4cb4d92
Hikmahx <hikmayousuph@gmail.com>
2025-09-09 18:41:09 +0100

    querying enabled in GET posts route

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,22 +2,51 @@ import { prisma } from '@/lib/prisma'
 import { requireAdmin } from '@/lib/utils/auth'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url)
+  const page = parseInt(searchParams.get('page') || '1')
+  const sortBy = searchParams.get('sortBy') || 'date'
+  const searchTerm = searchParams.get('search') || ''
+  const published = searchParams.get('published') || true
+
+  const POST_PER_PAGE = 2
+
+  const orderBy =
+    sortBy === 'title'
+      ? { title: 'asc' as const }
+      : { createdAt: 'desc' as const }
+  const publishedBool = published === 'true'
+
+  const query = {
+    take: POST_PER_PAGE,
+    skip: POST_PER_PAGE * (page - 1),
+    orderBy,
+    where: {
+      published: publishedBool,
+      OR: [
+        { title: { contains: searchTerm, mode: 'insensitive' as const } },
+        { content: { contains: searchTerm, mode: 'insensitive' as const } },
+        { description: { contains: searchTerm, mode: 'insensitive' as const } },
+      ],
+    },
+  }
+
   try {
-    const posts = await prisma.post.findMany({
-      include: {
-        author: {
-          select: { name: true, image: true },
+    const [posts, count] = await prisma.$transaction([
+      prisma.post.findMany({
+        ...query,
+        include: {
+          author: {
+            select: { name: true, image: true },
+          },
         },
-      }
-    })
-    // console.log(posts)
-    return NextResponse.json({ posts, status: 200 })
+      }),
+      prisma.post.count({ where: query.where }),
+    ])
+    console.log(posts, count)
+    return NextResponse.json({ posts, count, status: 200 })
   } catch (err) {
     console.log(err)
-    // return new NextResponse(
-    //   JSON.stringify({ message: 'Something went wrong', status: 500 })
-    // )
     return NextResponse.json({ message: 'Something went wrong', status: 500 })
   }
 }
